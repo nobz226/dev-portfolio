701eff504488c89b23b21244b94c997e25040b29
nobz226 <eduard.rotaru89@gmail.com>
2026-07-03 00:29:31 -0700

    Fixed Scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Loader from '@/components/Loader'
@@ -13,6 +20,7 @@ const Contact = lazy(() => import('@/pages/Contact'))
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col bg-snow">
         <Header />
         <div className="flex-1">
